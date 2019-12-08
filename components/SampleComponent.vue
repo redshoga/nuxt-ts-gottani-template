9c01425383d275667f8b4d02b82f594ed26add97
redshoga <yui.ichioka.contact@gmail.com>
2019-12-08 13:52:16 +0900

    Simplify default component

---
+++ components/SampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    value: {{ value }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    value: {
+      type: Number,
+      default: 0,
+      required: true
+    }
+  }
+})
+</script>
